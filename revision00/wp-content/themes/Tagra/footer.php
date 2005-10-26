9503926240037824cd00e26eb05a8acde3916951
steamedpenguin <steamedpenguin@e8f08b0e-a5ea-0310-8095-fb99c73dbebe>
2005-10-26 23:39:08 +0000

    new steampress import. Based on WP 1.5.2

---
+++ revision00/wp-content/themes/Tagra/footer.php
@@ -0,0 +1,13 @@
+
+<hr />
+<div id="footer">
+	<p>Lay me under the Oranges of Yafa; to be first to see their sadness turn to joy.</p>
+</div>
+</div>
+
+<?php /* "Just what do you think you're doing Dave?" */ ?>
+
+		<?php wp_footer(); ?>
+
+</body>
+</html>
