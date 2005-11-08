6fd2a7a9b682c737aef6179cce6525c4865fc2ed
steamedpenguin <steamedpenguin@e8f08b0e-a5ea-0310-8095-fb99c73dbebe>
2005-11-08 14:33:01 +0000

    Admin section changes
    Stylesheet: Basic, gray, and not very polished. Will add polish as needed;
    
    Structure: Simplified some adminmenu choices as they make no sense within the SteamPress philosophy;
    
    Markup and code: Cleaned up some PHP ( <? if ():?> <?php endif; ?> ) is fucking stupid; cleaned up markup where needed; removed several options relating to how 
    many rows you have in the editing section, and other trivialities; Made a big change to the table markup in the link editing section. Separated link config 
    from link presentation; Note to self, when outputting HTML via PHP make sure that the markup comes out to match the indentation of markup already present; 
    throwing in an extra tab or newline never killed anyone, especially considering how bloated some of the code already is.

---
+++ trunk/wp-admin/admin-footer.php
@@ -0,0 +1,16 @@
+<div id="footer">
+	<p>
+		<a href="http://steamedpenguin.com/projects/steampress/">SteamPress</a>
+		<?php bloginfo('version'); ?> &#8212;
+		<a href="http://codex.wordpress.org/">
+			<?php _e('Documentation'); ?>
+		</a> &#8212;
+		<a href="http://wordpress.org/support/"><?php _e('Support Forums'); ?></a>
+</p>
+
+</div>
+
+<?php do_action('admin_footer', ''); ?>
+
+</body>
+</html>
