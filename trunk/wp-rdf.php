1a03cc03057abf8eff380b30ec06f3f26fcd23fd
steamedpenguin <steamedpenguin@e8f08b0e-a5ea-0310-8095-fb99c73dbebe>
2005-11-09 05:26:59 +0000

    clean brackets. Add brackets. Whitespace makes code legible. get rid of 'if (): endif;'

--- trunk/wp-rdf.php
+++ trunk/wp-rdf.php
@@ -1,10 +1,11 @@
 <?php /* RDF 1.0 generator, original version by garym@teledyn.com */
 
-if (empty($feed)) {
-    $blog = 1; // enter your blog's ID
-		$feed = 'rdf';
-    $doing_rss = 1;
-    require('wp-blog-header.php');
+if (empty($feed))
+{
+	$blog = 1; // enter your blog's ID
+	$feed = 'rdf';
+	$doing_rss = 1;
+	require('wp-blog-header.php');
 }
 
 header('Content-type: application/rdf+xml; charset=' . get_settings('blog_charset'), true);
@@ -12,7 +13,7 @@ $more = 1;
 
 ?>
 <?php echo '<?xml version="1.0" encoding="'.get_settings('blog_charset').'"?'.'>'; ?>
-<!-- generator="wordpress/<?php echo $wp_version ?>" -->
+<!-- generator="steampress/<?php echo $wp_version ?>" -->
 <rdf:RDF
 	xmlns="http://purl.org/rss/1.0/"
 	xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
@@ -32,26 +33,60 @@ $more = 1;
 	<sy:updateBase>2000-01-01T12:00+00:00</sy:updateBase>
 	<items>
 		<rdf:Seq>
-		<?php $items_count = 0; if ($posts) { foreach ($posts as $post) { start_wp(); ?>
+<?php
+$items_count = 0;
+if ($posts)
+{
+	foreach ($posts as $post)
+	{
+		start_wp();
+?>
 			<rdf:li rdf:resource="<?php permalink_single_rss() ?>"/>
-		<?php $wp_items[] = $row; $items_count++; if (($items_count == get_settings('posts_per_rss')) && empty($m)) { break; } } } ?>
+<?php
+		$wp_items[] = $row;
+		$items_count++;
+		if (($items_count == get_settings('posts_per_rss')) && empty($m))
+		{
+			break;
+		}
+	}
+}
+?>
 		</rdf:Seq>
 	</items>
 </channel>
-<?php if ($posts) { foreach ($posts as $post) { start_wp(); ?>
+<?php
+if ($posts)
+{
+	foreach ($posts as $post)
+	{
+		start_wp();
+?>
 <item rdf:about="<?php permalink_single_rss() ?>">
 	<title><?php the_title_rss() ?></title>
 	<link><?php permalink_single_rss() ?></link>
-	 <dc:date><?php echo mysql2date('Y-m-d\TH:i:s\Z', $post->post_date_gmt, false); ?></dc:date>
+	<dc:date><?php echo mysql2date('Y-m-d\TH:i:s\Z', $post->post_date_gmt, false); ?></dc:date>
 	<dc:creator><?php the_author() ?></dc:creator>
 	<?php the_category_rss('rdf') ?>
-<?php if (get_settings('rss_use_excerpt')) : ?>
+<?php
+		if (get_settings('rss_use_excerpt'))
+		{
+?>
 	<description><?php the_excerpt_rss() ?></description>
-<?php else : ?>
+<?php
+		}
+		else
+		{
+?>
 	<description><?php the_content_rss('', 0, '', get_settings('rss_excerpt_length'), 2) ?></description>
 	<content:encoded><![CDATA[<?php the_content('', 0, '') ?>]]></content:encoded>
-<?php endif; ?>
-	
+<?php
+		}
+?>
+
 </item>
-<?php } }  ?>
+<?php
+	}
+}
+?>
 </rdf:RDF>
