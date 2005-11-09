0f56e0652c587307c4242011dd208b8d20bbed79
steamedpenguin <steamedpenguin@e8f08b0e-a5ea-0310-8095-fb99c73dbebe>
2005-11-09 05:28:31 +0000

    Add brackets. Whitespace makes code legible.

--- trunk/wp-pass.php
+++ trunk/wp-pass.php
@@ -2,7 +2,9 @@
 require( dirname(__FILE__) . '/wp-config.php');
 
 if ( get_magic_quotes_gpc() )
+{
 	$_POST['post_password'] = stripslashes($_POST['post_password']);
+}
 
 // 10 days
 setcookie('wp-postpass_' . COOKIEHASH, $_POST['post_password'], time() + 864000, COOKIEPATH);
