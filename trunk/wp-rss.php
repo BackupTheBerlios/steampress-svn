a20d81a17f7b2f52e619e4bd0695545a3779ff61
steamedpenguin <steamedpenguin@e8f08b0e-a5ea-0310-8095-fb99c73dbebe>
2005-11-09 05:07:42 +0000

    clean brackets. Add brackets. Whitespace makes code legible.

--- trunk/wp-rss.php
+++ trunk/wp-rss.php
@@ -1,10 +1,11 @@
 <?php
 
-if (empty($feed)) {
-    $blog = 1;
-		$feed = 'rss';
-    $doing_rss = 1;
-    require('wp-blog-header.php');
+if (empty($feed))
+{
+	$blog = 1;
+	$feed = 'rss';
+	$doing_rss = 1;
+	require('wp-blog-header.php');
 }
 
 header('Content-type: text/xml; charset=' . get_settings('blog_charset'), true);
@@ -22,16 +23,40 @@ $more = 1;
 	<docs>http://backend.userland.com/rss092</docs>
 	<language><?php echo get_option('rss_language'); ?></language>
 
-<?php $items_count = 0; if ($posts) { foreach ($posts as $post) { start_wp(); ?>
+<?php
+$items_count = 0;
+if ($posts)
+{
+	foreach ($posts as $post)
+	{
+		start_wp();
+?>
 	<item>
 		<title><?php the_title_rss() ?></title>
-<?php if (get_settings('rss_use_excerpt')) { ?>
+<?php
+		if (get_settings('rss_use_excerpt'))
+		{
+?>
 		<description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
-<?php } else { // use content ?>
+<?php
+		}
+		else
+		{ // use content
+?>
 		<description><?php the_content_rss('', 0, '', get_settings('rss_excerpt_length')) ?></description>
-<?php } ?>
+<?php
+		}
+?>
 		<link><?php permalink_single_rss() ?></link>
 	</item>
-<?php $items_count++; if (($items_count == get_settings('posts_per_rss')) && empty($m)) { break; } } } ?>
+<?php
+		$items_count++;
+		if (($items_count == get_settings('posts_per_rss')) && empty($m))
+		{
+			break;
+		}
+	}
+}
+?>
 </channel>
 </rss>
